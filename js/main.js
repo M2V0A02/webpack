23a086c133d7f53bf7eebe624039991b83ebdfd7
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-24 11:49:07 +0500

    создал unit.js

---
+++ js/main.js
@@ -0,0 +1,2 @@
+import {Unit} from "./unit.js";
+let test = new Unit("peasant", 120, 80, 60, 20, 20, 10, 5, 20, 20);
---
+++ js/unit.js
@@ -0,0 +1,15 @@
+export class Unit{
+    constructor(name, size, health, leadership, meleeAttack, meleeDefence,
+        chargeBonus, weaponDamageArmourPiercing, weaponDamage, armor){
+        this.name = name;
+        this.size = size;
+        this.health = health;
+        this.leadership = leadership;
+        this.meleeAttack = meleeAttack;
+        this.meleeDefence = meleeDefence;
+        this.chargeBonus = chargeBonus;
+        this.weaponDamage = weaponDamage;
+        this.weaponDamageArmourPiercing = weaponDamageArmourPiercing;
+        this.armor = armor;
+    }; 
+}
