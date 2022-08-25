849c89e104bbfe90bba7dbdfabf4036c087fbb18
m2v0a02 <volodin.mixa2015@yandex.ru>
2022-08-25 14:14:53 +0500

    добавил css Loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import {Unit} from "./unit";
 import {createAnalytics} from "./analytics";
+import "../css/style.css";
 window.analytics = createAnalytics()
 let test = new Unit("peasant", 120, 80, 60, 20, 20, 10, 5, 20, 20);
 content.innerHTML = test.toString();
