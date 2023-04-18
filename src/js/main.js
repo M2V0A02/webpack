192b001c2c90d7f5b5ac667615e13836ae75e719
M2V0A02 <volodin.mixa2015@yandex.ru>
2023-04-18 21:16:56 +0500

    Исправил ошибку

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,2 @@
-import {Unit} from "./unit";
-import {createAnalytics} from "./analytics";
-import "../css/style.css";
-window.analytics = createAnalytics()
-let test = new Unit("peasant", 120, 80, 60, 20, 20, 10, 5, 20, 20);
-content.innerHTML = test.toString();
+import '../css/style.scss';
+console.log(1)
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,16 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: '/src/js/main.js',
-    devtool: "source-map",
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist', 'js'),
     },
     module: {
         rules: [
             {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-                },
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader",  "sass-loader"],
             },
         
         ]
